b05562422d5956ec151ce15e0eabe4146fded678
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2021-08-01 13:48:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
-bot_token = "1858868999:AAEj-7mlndlKEsDkkQc1hrYcZB0dBTEMMGM" # yourbot token
-ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" # Get this from @ARQBot
-api_id = 7193256 # your api id
-api_hash = "34d4f8d8823821c099d984eb342d42ce" # your apihash
-# List of supported languages >>
-# https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
-# Leave it as it is
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot_token = getenv("BOT_TOKEN", "session")
+ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" 
+api_id = getenv("API_ID", "api_id") 
+api_hash = getenv("API_HASH", "api_hash")
 LANGUAGE = "en"
-# Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
