b21c4350a7f918a3c40b21b946285dfd9cc6cc2e
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2021-08-01 15:37:06 +0530

    Update rika.py

--- rika.py
+++ rika.py
@@ -6,7 +6,7 @@ from aiohttp import ClientSession
 from pyrogram import (Client, filters, idle)
 from Python_ARQ import ARQ
 
-from config import bot, BOT_TOKEN
+from config import bot, BOT_TOKEN, ARQ_API_KEY, ARQ_API_BASE_URL
 bot_token= BOT_TOKEN
 
 print("[INFO]: Master EliteDarkRay checking...")
