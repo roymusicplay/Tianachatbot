da259276b817aedb08e011a0e5834716f7dfe305
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2021-08-01 15:05:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
-from os import getenv
+import os
 
-from dotenv import load_dotenv
+from heroku3 import from_key
+from pyrogram import Client
+from pyromod import listen
 
-load_dotenv()
-
-bot_token = getenv("BOT_TOKEN", "session")
+api_id = int(os.environ.get("API_ID", 0))
+api_hash = os.environ.get("API_HASH", None)
+bot_token = os.environ.get("BOT_TOKEN", None)
 ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" 
-api_id = getenv("API_ID", "api_id") 
-api_hash = getenv("API_HASH", "api_hash")
 LANGUAGE = "en"
 ARQ_API_BASE_URL = "https://thearq.tech"
+
+bot = Client(":memory:",
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN)
